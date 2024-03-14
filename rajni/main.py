55a66a3aa93a065804564b80b84d8aac757a7c74
Mehak Kashyap <mehakashyap7890@gmail.com>
2024-03-14 21:50:53 +0530

    add links

--- rajni/main.py
+++ rajni/main.py
@@ -1,10 +1,4 @@
-num1 = int(input("1st: "))
-num2 = int(input("2nd: "))
-num3 = int(input("3rd: "))
-
-if (num1 > num2) and (num1 > num3):
-    print("num1 is greater")
-elif (num2 > num1) and (num2 > num3):
-    print("num2 is greater")
-else:
-    print("num3 is greater")
+def greet(name):
+    print("Hello",name)
+    
+greet("Mehak")
